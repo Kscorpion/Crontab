4bf6f9b82eff19c6ee7b5e0003429fafd353d0dd
Kscorpion <thelastsun@163.com>
2020-05-28 22:36:58 +0800

    fix 强杀时etcd目录错误问题

--- master/JobMgr.go
+++ master/JobMgr.go
@@ -136,26 +136,28 @@ func (jobMgr *JobMgr) ListJobs() (jobList []*common.Job, err error) {
 	return
 }
 
-//杀死任务
-func (JobMgr *JobMgr) KillJob(name string) (err error) {
-	//更新/cron/killer/任务名
+// 杀死任务
+func (jobMgr *JobMgr) KillJob(name string) (err error) {
+	// 更新一下key=/cron/killer/任务名
 	var (
 		killerKey      string
-		leaseGreanResp *clientv3.LeaseGrantResponse
+		leaseGrantResp *clientv3.LeaseGrantResponse
 		leaseId        clientv3.LeaseID
 	)
 
-	//通知worker杀死对应任务
-	killerKey = common.JOB_KILL_DIR + name
+	// 通知worker杀死对应任务
+	killerKey = common.JOB_KILLER_DIR + name
 
-	//让worker监听到一次put操作,创建一个租约让其自动过期即可
-	if leaseGreanResp, err = JobMgr.lease.Grant(context.TODO(), 1); err != nil {
+	// 让worker监听到一次put操作, 创建一个租约让其稍后自动过期即可
+	if leaseGrantResp, err = jobMgr.lease.Grant(context.TODO(), 1); err != nil {
 		return
 	}
-	//租约ID
-	leaseId = leaseGreanResp.ID
-	//设置killer标记
-	if _, err = JobMgr.kv.Put(context.TODO(), killerKey, "", clientv3.WithLease(leaseId)); err != nil {
+
+	// 租约ID
+	leaseId = leaseGrantResp.ID
+
+	// 设置killer标记
+	if _, err = jobMgr.kv.Put(context.TODO(), killerKey, "", clientv3.WithLease(leaseId)); err != nil {
 		return
 	}
 	return
