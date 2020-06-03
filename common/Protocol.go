b0aa63a30458323d7c6bcebf3fb70818b7c20c61
Kscorpion <thelastsun@163.com>
2020-06-03 21:07:54 +0800

    解决日志格式问题

--- common/Protocol.go
+++ common/Protocol.go
@@ -8,52 +8,51 @@ import (
 	"time"
 )
 
-//定时任务
 type Job struct {
-	Name     string `json:"name"`     //任务名
-	Command  string `json:"command"`  //shell命令
-	CronExpr string `json:"cronExpr"` //cron表达式
+	Name     string `json:"name"`     //  任务名
+	Command  string `json:"command"`  // shell命令
+	CronExpr string `json:"cronExpr"` // cron表达式
 }
 
-//任务调度计划
-
+// 任务调度计划
 type JobSchedulePlan struct {
-	Job      *Job                 //调度的任务信息
-	Expr     *cronexpr.Expression //解析好的cronexpr表达式
-	NextTime time.Time            //下次调度时间
+	Job      *Job                 // 要调度的任务信息
+	Expr     *cronexpr.Expression // 解析好的cronexpr表达式
+	NextTime time.Time            // 下次调度时间
 }
 
+// 任务执行状态
 type JobExecuteInfo struct {
-	Job        *Job               //任务信息
-	PlanTime   time.Time          //理论上调度时间
-	RealTime   time.Time          //实际上调度时间
-	CancelFunc context.CancelFunc //用于取消command执行的cancel函数
-	CancelCtx  context.Context    //任务command的上下文
-
+	Job        *Job               // 任务信息
+	PlanTime   time.Time          // 理论上的调度时间
+	RealTime   time.Time          // 实际的调度时间
+	CancelCtx  context.Context    // 任务command的context
+	CancelFunc context.CancelFunc //  用于取消command执行的cancel函数
 }
 
-//HTTP接口应答
+// HTTP接口应答
 type Response struct {
 	Errno int         `json:"errno"`
 	Msg   string      `json:"msg"`
 	Data  interface{} `json:"data"`
 }
 
-//变化事件
+// 变化事件
 type JobEvent struct {
-	EventType int //SAVE,DELETE
+	EventType int //  SAVE, DELETE
 	Job       *Job
 }
 
-//任务执行结果
+// 任务执行结果
 type JobExecuteResult struct {
-	ExecuteInfo *JobExecuteInfo //执行状态
-	OutPut      []byte          //脚本输出
-	Err         error           //脚本错误原因
-	StartTime   time.Time       //脚本启动时间
-	EndTime     time.Time       //脚本结束时间
+	ExecuteInfo *JobExecuteInfo // 执行状态
+	Output      []byte          // 脚本输出
+	Err         error           // 脚本错误原因
+	StartTime   time.Time       // 启动时间
+	EndTime     time.Time       // 结束时间
 }
 
+// 任务执行日志
 type JobLog struct {
 	JobName      string `json:"jobName" bson:"jobName"`           // 任务名字
 	Command      string `json:"command" bson:"command"`           // 脚本命令
@@ -65,41 +64,43 @@ type JobLog struct {
 	EndTime      int64  `json:"endTime" bson:"endTime"`           // 任务执行结束时间
 }
 
-//日志批次
+// 日志批次
 type LogBatch struct {
-	Logs []interface{} //多条日志
-
+	Logs []interface{} // 多条日志
 }
 
-//任务日志排序规则
-type SortLogByStartTime struct {
-	SortOrder int `bson:"startTime"` //按照startTime: -1
-}
-
-//任务日志过滤条件
+// 任务日志过滤条件
 type JobLogFilter struct {
-	JobName string `bson:"jobname"`
+	JobName string `bson:"jobName"`
 }
 
-//应答方法
+// 任务日志排序规则
+type SortLogByStartTime struct {
+	SortOrder int `bson:"startTime"` // {startTime: -1}
+}
+
+// 应答方法
 func BuildResponse(errno int, msg string, data interface{}) (resp []byte, err error) {
-	//1.定义一个response
+	// 1, 定义一个response
 	var (
 		response Response
 	)
+
 	response.Errno = errno
 	response.Msg = msg
 	response.Data = data
-	//2.序列化json
+
+	// 2, 序列化json
 	resp, err = json.Marshal(response)
 	return
 }
 
-//反序列化job
+// 反序列化Job
 func UnpackJob(value []byte) (ret *Job, err error) {
 	var (
 		job *Job
 	)
+
 	job = &Job{}
 	if err = json.Unmarshal(value, job); err != nil {
 		return
@@ -108,18 +109,18 @@ func UnpackJob(value []byte) (ret *Job, err error) {
 	return
 }
 
-//从etcd的可以提取任务名
+// 从etcd的key中提取任务名
 // /cron/jobs/job10抹掉/cron/jobs/
 func ExtractJobName(jobKey string) string {
 	return strings.TrimPrefix(jobKey, JOB_SAVE_DIR)
 }
 
-// /cron/killer/job10抹掉/cron/jobs/
+// 从 /cron/killer/job10提取job10
 func ExtractKillerName(killerKey string) string {
 	return strings.TrimPrefix(killerKey, JOB_KILLER_DIR)
 }
 
-//任务变化事件两种:1)更新任务 2）删除任务
+// 任务变化事件有2种：1）更新任务 2）删除任务
 func BuildJobEvent(eventType int, job *Job) (jobEvent *JobEvent) {
 	return &JobEvent{
 		EventType: eventType,
@@ -127,16 +128,18 @@ func BuildJobEvent(eventType int, job *Job) (jobEvent *JobEvent) {
 	}
 }
 
-//构造任务执行计划
+// 构造任务执行计划
 func BuildJobSchedulePlan(job *Job) (jobSchedulePlan *JobSchedulePlan, err error) {
 	var (
 		expr *cronexpr.Expression
 	)
-	//解析JOB的cron表达式
+
+	// 解析JOB的cron表达式
 	if expr, err = cronexpr.Parse(job.CronExpr); err != nil {
 		return
 	}
-	//生成任务调度计划对象
+
+	// 生成任务调度计划对象
 	jobSchedulePlan = &JobSchedulePlan{
 		Job:      job,
 		Expr:     expr,
@@ -145,12 +148,12 @@ func BuildJobSchedulePlan(job *Job) (jobSchedulePlan *JobSchedulePlan, err error
 	return
 }
 
-//构造执行状态信息
+// 构造执行状态信息
 func BuildJobExecuteInfo(jobSchedulePlan *JobSchedulePlan) (jobExecuteInfo *JobExecuteInfo) {
 	jobExecuteInfo = &JobExecuteInfo{
 		Job:      jobSchedulePlan.Job,
-		PlanTime: jobSchedulePlan.NextTime, //计算调度时间
-		RealTime: time.Now(),               //真实调度时间
+		PlanTime: jobSchedulePlan.NextTime, // 计算调度时间
+		RealTime: time.Now(),               // 真实调度时间
 	}
 	jobExecuteInfo.CancelCtx, jobExecuteInfo.CancelFunc = context.WithCancel(context.TODO())
 	return
