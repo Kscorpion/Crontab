6ee9ad606027e717c8f19d466534636bacd33ecf
Kscorpion <thelastsun@163.com>
2020-06-11 00:05:01 +0800

    增加获取健康worker节点ip列表的API

--- master/ApiServer.go
+++ master/ApiServer.go
@@ -181,6 +181,28 @@ ERR:
 	}
 }
 
+//获取健康worker节点ip列表
+func handleWorkerList(resp http.ResponseWriter, req *http.Request) {
+	var (
+		workerArr []string
+		err       error
+		bytes     []byte
+	)
+	if workerArr, err = G_workerMgr.ListWorkers(); err != nil {
+		goto ERR
+	}
+	// 正常应答
+	if bytes, err = common.BuildResponse(0, "success", workerArr); err == nil {
+		resp.Write(bytes)
+	}
+	return
+
+ERR:
+	if bytes, err = common.BuildResponse(-1, err.Error(), nil); err == nil {
+		resp.Write(bytes)
+	}
+}
+
 //初始化服务
 func InitApiServer() (err error) {
 	var (
@@ -197,6 +219,7 @@ func InitApiServer() (err error) {
 	mux.HandleFunc("/job/list", handleJobList)
 	mux.HandleFunc("/job/kill", handleJobKill)
 	mux.HandleFunc("/job/log", handleJobLog)
+	mux.HandleFunc("/worker/list", handleWorkerList)
 
 	//静态文件目录
 	staticDir = http.Dir(G_config.Webroot)
